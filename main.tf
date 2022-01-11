c8c828639f079fac04aa9997c10f37c22cfdafc5
Georgemcruddiman <georgeruddiman@gmail.com>
2022-01-11 21:34:22 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,15 +12,15 @@ resource "azurerm_resource_group" "rg-dev" {
 resource "azurerm_log_analytics_workspace" "law-dev" {
   name = "grtest-dev-law"
   location = "westus2"
-  resource_group_name = "azurerm_resource_group.rg-dev.name"
+  resource_group_name = azurerm_resource_group.rg-dev.name
   tags = {"Bill-To" = "Group Infrastructure - Hosting"}
 }
 
-#resource "azurerm_application_insights" "insights-dev" {
-#  name = "grtest-dev-app-insights"
-#  location = "westus2"
-#  resource_group_name = "azurerm_resource_group.rg-dev.name"
-#  application_type = "web"
-#  workspace_id = "azurerm_log_analytics_workspace.law-dev.id"
-#  tags = {"Bill-To" = "Group Infrastructure - Hosting"}
-#}
+resource "azurerm_application_insights" "insights-dev" {
+  name = "grtest-dev-app-insights"
+  location = "westus2"
+  resource_group_name = azurerm_resource_group.rg-dev.name
+  application_type = "web"
+  workspace_id = azurerm_log_analytics_workspace.law-dev.id
+  tags = {"Bill-To" = "Group Infrastructure - Hosting"}
+}
